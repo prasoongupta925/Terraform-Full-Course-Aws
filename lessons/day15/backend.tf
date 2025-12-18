9b23acf216a1844fcf9fc6b6813dc3deae1123b0
Prasoon Gupta <prasoongupta925@gmail.com>
2025-12-18 17:21:24 +0530

    Update backend.tf

--- lessons/day15/backend.tf
+++ lessons/day15/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket-piyushsachdeva"
+    bucket = "prasoon-gupta-terraform-learning-bucket.tfstate"
     key    = "lessons/day15/terraform.tfstate"
     region = "us-east-1"
   }
